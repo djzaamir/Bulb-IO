bca143b8f37eec39a935ab977373c61a6381ee9f
djzaamir <djzaamir@gmail.com>
2019-12-16 22:19:11 +0500

    Added different type of error handling while handling Volley exceptions

--- app/src/main/java/com/andromeda/djzaamir/bulb_io/MainActivity.java
+++ app/src/main/java/com/andromeda/djzaamir/bulb_io/MainActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -20,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView status_text;
     private Button toggle_button;
     private RequestQueue volley_request_queue;
-    private String MSG_PROCESSING = "Processing...";
+    private String MSG_PROCESSING   = "Getting Status...";
+    private final String STATUS_ON  = "Status : ON";
+    private final String STATUS_OFF = "Status : OFF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //get UI references
-        status_text   = findViewById(R.id.status_text);
+        status_text = findViewById(R.id.status_text);
         toggle_button = findViewById(R.id.toggle_button);
 
         //Perform any pre-processing on the UI
@@ -36,41 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
         //instantiate volley request queue
         volley_request_queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.0.111/status";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (Integer.valueOf(response) == 0){
-                    status_text.setText("ON");
-                }else if (Integer.valueOf(response) == 1){
-                    status_text.setText("OFF");
-                }
-            }
-        },
-        new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                status_text.setText("Error : " + error.networkResponse.statusCode);
-            }
-        });
 
-        //add to volley background thread queue
-        volley_request_queue.add(stringRequest);
+        sendVolleyRequest( "http://192.168.0.111/status");
     }
 
     public void sendToggle(View view) {
+        sendVolleyRequest("http://192.168.0.111/toggle");
+    }
 
+    private void sendVolleyRequest(String url) {
         toggle_button.setEnabled(false);
 
         status_text.setText(MSG_PROCESSING);
-        String url = "http://192.168.0.111/toggle";
         StringRequest bulb_toggle_request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                if (Integer.valueOf(response) == 0){
-                    status_text.setText("ON");
-                }else if (Integer.valueOf(response) == 1){
-                    status_text.setText("OFF");
+                if (Integer.valueOf(response) == 0) {
+                    status_text.setText(STATUS_ON);
+                } else if (Integer.valueOf(response) == 1) {
+                    status_text.setText(STATUS_OFF);
                 }
                 toggle_button.setEnabled(true);
             }
@@ -78,10 +68,23 @@ public class MainActivity extends AppCompatActivity {
         new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                status_text.setText("Error : " + error.networkResponse.statusCode);
+                if (error instanceof TimeoutError){
+                    status_text.setText("Connection Timeout");
+                }else if(error instanceof NoConnectionError){
+                    status_text.setText("No Connect");
+                }else if(error instanceof ServerError){
+                    status_text.setText("Server Error");
+                }else if(error instanceof NetworkError){
+                    status_text.setText("Network Error");
+                }else if(error instanceof ParseError){
+                    status_text.setText("Parse Error");
+                }
                 status_text.setEnabled(true);
             }
         });
+
+
+        //Add to volley queue, so it may be processed
         volley_request_queue.add(bulb_toggle_request);
     }
 }
